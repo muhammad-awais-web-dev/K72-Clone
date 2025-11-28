37a9796a06fa6509c0c6fffc772fd75764376391
Muhammad Awais <awaisrafique4929@gmail.com>
2025-11-28 14:35:34 +0500

    I sample for the project page animation has been made

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -36,7 +36,6 @@ const Navbar = (props) => {
     const actualLinks = gsap.utils.toArray(
       LinksContainerRef.current.querySelectorAll(".actualLinks")
     );
-    console.log(actualLinks);
     linksAnimated.forEach((link) => {
       const linksAnimatedInner = gsap.utils.toArray(
         link.querySelectorAll(".links-animated-inner")
@@ -90,21 +89,34 @@ const Navbar = (props) => {
       onMove: () => {},
       onDown: () => {
         setDFactor(-1);
-        console.log(-1);
       },
       onUp: () => {
         setDFactor(1);
-        console.log(1);
       },
     });
   });
 
   useGSAP(() => {
+    const Links = gsap.utils.toArray(
+      LinksContainerRef.current.querySelectorAll(".actualLinks")
+    );
     if (open !== null) {
       if (open === "Full") {
         // Opening animation
         stairsInOut(stairsParent.current, () => {
           FullPageRef.current.style.display = "flex";
+          gsap.from(Links,{
+            rotateX: -90,
+            duration: 1,
+            opacity: 0,
+            stagger: 0.2,
+          })
+          gsap.from(LinksContainerRef.current.querySelectorAll(".K72-Logo,.Close-button"),{
+            delay: 1.5,
+            y: -100,
+            opacity: 0,
+            duration: 2,
+          })
         });
       } else if (open === "close") {
         // Closing animation
@@ -157,7 +169,7 @@ const Navbar = (props) => {
       <div className="topBar">
         <div
           onClick={() => navigate("/")}
-          className=" cursor-pointer absolute p-5 border-2 border-white -mt-2 -ml-2 top-0 left-0"
+          className=" K72-Logo cursor-pointer absolute p-5 border-2 border-white -mt-2 -ml-2 top-0 left-0"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -166,7 +178,7 @@ const Navbar = (props) => {
             viewBox="0 0 103 44"
           >
             <path
-              fill="#ffffff"
+              // fill="#ffffff"
               d="M35.1441047,8.4486911 L58.6905011,8.4486911 L58.6905011,-1.3094819e-14 L35.1441047,-1.3094819e-14 L35.1441047,8.4486911 Z M20.0019577,0.000230366492 L8.83414254,25.3433089 L18.4876971,25.3433089 L29.5733875,0.000230366492 L20.0019577,0.000230366492 Z M72.5255345,0.000691099476 L72.5255345,8.44846073 L94.3991559,8.44846073 L94.3991559,16.8932356 L72.5275991,16.8932356 L72.5275991,19.5237906 L72.5255345,19.5237906 L72.5255345,43.9274346 L102.80937,43.9274346 L102.80937,35.4798953 L80.9357483,35.4798953 L80.9357483,25.3437696 L94.3996147,25.3428482 L94.3996147,16.8953089 L102.80937,16.8953089 L102.80937,0.000691099476 L72.5255345,0.000691099476 Z M-1.30398043e-14,43.9278953 L8.78642762,43.9278953 L8.78642762,0.0057591623 L-1.30398043e-14,0.0057591623 L-1.30398043e-14,43.9278953 Z M58.6849955,8.4486911 L43.1186904,43.9274346 L52.3166592,43.9274346 L67.9877996,8.4486911 L58.6849955,8.4486911 Z M18.4688864,25.3437696 L26.7045278,43.9278953 L36.2761871,43.9278953 L28.1676325,25.3375497 L18.4688864,25.3437696 Z"
             ></path>
           </svg>
@@ -207,7 +219,7 @@ const Navbar = (props) => {
       >
         <div
           onClick={() => navigate("/")}
-          className=" cursor-pointer absolute p-5 border-2 border-white -mt-2 -ml-2 top-0 left-0"
+          className="K72-Logo cursor-pointer absolute p-5 border-2 border-white -mt-2 -ml-2 top-0 left-0"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -216,7 +228,7 @@ const Navbar = (props) => {
             viewBox="0 0 103 44"
           >
             <path
-              fill="#ffffff"
+              // fill="#ffffff"
               d="M35.1441047,8.4486911 L58.6905011,8.4486911 L58.6905011,-1.3094819e-14 L35.1441047,-1.3094819e-14 L35.1441047,8.4486911 Z M20.0019577,0.000230366492 L8.83414254,25.3433089 L18.4876971,25.3433089 L29.5733875,0.000230366492 L20.0019577,0.000230366492 Z M72.5255345,0.000691099476 L72.5255345,8.44846073 L94.3991559,8.44846073 L94.3991559,16.8932356 L72.5275991,16.8932356 L72.5275991,19.5237906 L72.5255345,19.5237906 L72.5255345,43.9274346 L102.80937,43.9274346 L102.80937,35.4798953 L80.9357483,35.4798953 L80.9357483,25.3437696 L94.3996147,25.3428482 L94.3996147,16.8953089 L102.80937,16.8953089 L102.80937,0.000691099476 L72.5255345,0.000691099476 Z M-1.30398043e-14,43.9278953 L8.78642762,43.9278953 L8.78642762,0.0057591623 L-1.30398043e-14,0.0057591623 L-1.30398043e-14,43.9278953 Z M58.6849955,8.4486911 L43.1186904,43.9274346 L52.3166592,43.9274346 L67.9877996,8.4486911 L58.6849955,8.4486911 Z M18.4688864,25.3437696 L26.7045278,43.9278953 L36.2761871,43.9278953 L28.1676325,25.3375497 L18.4688864,25.3437696 Z"
             ></path>
           </svg>
@@ -247,7 +259,7 @@ const Navbar = (props) => {
               <Link
                 to={item.link}
                 key={index}
-                className="actualLinks relative w-full h-1/4 flex flex-col justify-center items-start overflow-hidden border-y -mt-1 bg-black border-white text-white"
+                className=" origin-top actualLinks relative w-full h-1/4 flex flex-col justify-center items-start overflow-hidden border-y -mt-1 bg-black border-white text-white"
               >
                 <div className="flex w-full justify-center items-center">
                   {item.name}
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import React, { useRef } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Projects = () => {
-  return <div className="text-6xl text-white">Projects</div>;
+  const projectRef = useRef(null);
+
+  useGSAP(() => {
+    if (!projectRef.current) return;
+
+    const sections = gsap.utils.toArray(projectRef.current.querySelectorAll('.project-item'));
+    
+    gsap.set(sections, { height: "20vh" });
+    
+    gsap.to(sections, {
+      height: "55vh",
+      ease: "none",
+      stagger: {
+        amount: 0.9,
+      },
+      scrollTrigger: {
+        trigger: projectRef.current,
+        start: "top 100%",
+        end: "top -150%",
+        scrub: 0,
+        markers: true,
+        invalidateOnRefresh: true,
+      }
+    });
+  }, { scope: projectRef });
+
+  return (
+    <div className="text-[8vw] font-[Lausanne-500] text-white pt-[50vh] px-5 ">
+      <h1 className="leading-[8vw]">Projets</h1>
+      <div ref={projectRef} id="Projects" className="h-[210vh] w-full my-5 gap-5 bg-amber-400 flex flex-col justify-start items-center">
+        <div className="project-item h-[55vh] hover:rounded-4xl overflow-hidden w-full bg-red-500"></div>
+        <div className="project-item h-[55vh] hover:rounded-4xl overflow-hidden w-full bg-blue-700"></div>
+        <div className="project-item h-[55vh] hover:rounded-4xl overflow-hidden w-full bg-red-500"></div>
+        <div className="project-item h-[55vh] hover:rounded-4xl overflow-hidden w-full bg-blue-700"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
